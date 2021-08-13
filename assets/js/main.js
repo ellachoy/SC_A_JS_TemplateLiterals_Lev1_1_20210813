93e900cc80da6b04e56f369c6f007874a5f1542b
Ella Choy <egal21de@gmail.com>
2021-08-13 20:51:53 +0200

    templateLiterals

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+console.log("hi")
+let Name="Ella";
+let Nachname="Choy";
+let Alter=46;
+let Geburtsort="Seoul in südKorea";
+let Groesse=162;
+let Gewicht=50;
+let hobbys="kochen";
+let Computerspile="Lily´s Garten";
+let Liblingsessen="scharfes"
+let Lieblingssport="Yoga, schwimmen";
+let Lieblingsmodemarke="undefined";
+let Lieblingsjahreszeit="Sommer";
+let Lieblingsurlaubsort="Thailand";
+
+document.write(`mein Name ist ${Name} ${Nachname} . bin ${Alter}Jahre Alt.<br> ich bin in ${Geburtsort}  geboren.
+ <br> Ich bin ${Groesse} gross.<br> mein hobby ist ${hobbys}. spiele gerne ${Computerspile} mit Computer.
+  <br> ich esse gerne , sehr gerne ${Liblingsessen} natürlich koreanish ist mein favorit aber ich liebe 
+   auch original-thailändisches Küche und vietnamisches. ummmm lekker leker. <br> Mein Liblingssport ist ${Lieblingssport}. durch ${Lieblingssport} habe ich mein chronisches Schmerzen am Rücken und Nacken etwas schonen können. 
+   <br> Meine Lieblingsjahreszeit ist ${ Lieblingsjahreszeit}. ich liebe Sonne deshalb mein lieblingsUrlabsort ist ${Lieblingsurlaubsort}.<br> Nachdem Bootcamp ich und mein Mann werden in Thiland schönen Urlaub haben.`);
